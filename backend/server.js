0bc5bdfacab233ec316c70c2493c76f3d10a0397
Keamogetswe Monama <keamonama077@gmail.com>
2025-12-21 22:11:51 +0200

    Initialize Express backend and connect frontend login to API

--- backend/server.js
+++ backend/server.js
@@ -1,35 +1,45 @@
 const express = require("express");
 const fs = require("fs");
-const { json } = require("stream/consumers");
+const cors = require("cors");
+const path = require("path");
+//const { json } = require("stream/consumers");
 
 const app = new express();
+app.use(cors());
 
 const getLogin = (req, res) => {
-  if (req.query.role === "tenant") {
-    const data = fs.readFileSync("./data/tenants.json", "utf-8");
-    const tenants = json.parse(data);
+  if (req.query.role === "TENANT") {
+    //To debug
+    console.log("LOGIN HIT:", req.query);
+
+    const tenantsPath = path.join(__dirname, "..", "frontend/data", "tenants.json");
+    const data = fs.readFileSync(tenantsPath, "utf-8");
+
+    const tenants = JSON.parse(data);
     const tenantExists = tenants.find(
-      (tenant) => tenant.tenantCode === req.query.tenantCode
+      (tenant) => tenant.tenantId === req.query.tenantCode
     );
     if (!tenantExists) {
-      res.code(404).json({ message: "User not found" });
+      return res.status(404).json({ message: "User not found" });
+    } else {
+      return res.status(200).json({ message: "User exists" });
     }
-  } else {
-    const data = fs.readFileSync("./data/owners.json", "utf-8");
-    const owners = json.parse(data);
-    const ownerExists = owners.find(
-      (owner) => owner.phone === req.query.phone
-    );
+  } else if (req.query.role === "OWNER") {
+    const ownersPath = path.join(__dirname, "..", "frontend/data", "owners.json");
+    const data = fs.readFileSync(ownersPath, "utf-8");
+    const owners = JSON.parse(data);
+    const ownerExists = owners.find((owner) => owner.phone === req.query.phone);
     if (!ownerExists) {
       return res.status(404).json({ message: "user not found" });
+    } else {
+      return res.status(200).json({ message: "user exist" });
     }
-    else{
-         return res.status(200).json({message: "user exist"});
-    }
+  } else {
+    return res.status(404).json({ message: "Invalid role" });
   }
 };
 
-app.get("api/login", getLogin);
+app.get("/api/login", getLogin);
 
 app.listen(3000, () => {
   console.log("SERVER IS RUNNING");
